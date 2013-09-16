6a8e19da2109b79239310820fb3bcd1233f40c53
Daniel <Drgreen_bg@yahoo.com>
2013-09-16 03:21:15 -0700

    Migrations fix

--- MathFightWebApi/MathFightWebApi.App/Global.asax.cs
+++ MathFightWebApi/MathFightWebApi.App/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MathFightWebApi.Data;
+using MathFightWebApi.Data.Migrations;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -16,6 +19,7 @@ namespace MathFightWebApi.App
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MathFightDbContext, Configuration>());
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
